ef8c4d9856878f7baf5c32c8303836311c0702f0
Mahendra <mahendrabrowny@gmail.com>
2023-05-16 09:48:10 +0530

    implement React-SWR for efficient data fetching

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["picsum.photos", "randomuser.me"],
+    domains: [
+      "picsum.photos",
+      "randomuser.me",
+      "pbs.twimg.com",
+      "wall-questing-assets.s3.amazonaws.com",
+    ],
   },
 };
 
